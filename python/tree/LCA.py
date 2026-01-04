a3f7b88a3fe74dcf1c44880ab6eda5466e5b816d
SangHyun Kim <vison901@gmail.com>
2026-01-04 21:50:18 +0900

    LCA

--- python/tree/LCA.py
+++ python/tree/LCA.py
@@ -7,9 +7,22 @@
 # p,q 는 모두 주어진 노드 트리에 속해있다
 #
 # 접근방식 : 각 노드가 처음으로 만나는 레벨의 노드를 찾는다
-# 케이스 구분 (노드 구조를 생각하기))
+#
+# 케이스 구분 (노드 구조를 이미지화 해본다)
 # case1: x -> x
 # case2: p || q -> 자기 자신 리턴 (p || q)
 # case3: left child && right child -> 자기 자신 리턴
 # case4: left child || right child -> left child || right child
+# * 위 조건대로 분기키시기 위해서는 자식노드의 리턴값을 '먼저' 알고 있어야 한다.
 
+def LCA(root, p, q):
+    if root == None:
+        return None
+    
+    left = LCA(root.left, p, q)
+    right = LCA(root.right, p, q)
+    if root == p or root == q:
+        return root
+    elif left and right:
+        return root
+    return left or right
